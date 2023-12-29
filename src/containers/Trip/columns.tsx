0cdaee035d14c3de87683190878406738b71293b
Jerad Maplethorpe <jeradmaplethorpe@gmail.com>
2023-12-29 15:51:45 -0600

    test lint-staged

--- src/containers/Trip/columns.tsx
+++ src/containers/Trip/columns.tsx
@@ -1,87 +1,91 @@
-import { ColumnDef } from "@tanstack/react-table"
-import { PackItem } from "@/types/pack"
+import { ColumnDef } from '@tanstack/react-table';
+
+import { Currency } from '@/lib/currencies';
+import { PackItem } from '@/types/pack';
+
 import {
-  QuantityCell,
   NotesCell,
-  WornCell,
+  QuantityCell,
   RemoveItemCell,
   WeightCell,
-} from "./cells"
-import { Currency } from "@/lib/currencies"
+  WornCell,
+} from './cells';
+
+// test linter
 
 export const columns = (currency: Currency): ColumnDef<PackItem>[] => [
   {
-    header: "Quantity",
+    header: 'Quantity',
     cell: ({ cell }) => <QuantityCell cell={cell} />,
     meta: {
-      style: { width: "10%" },
+      style: { width: '10%' },
     },
   },
   {
-    header: "Name",
-    accessorKey: "item.name",
+    header: 'Name',
+    accessorKey: 'item.name',
     meta: {
       style: {
-        width: "20%",
+        width: '20%',
       },
     },
   },
   {
-    header: "Product",
+    header: 'Product',
     accessorFn: ({ item }) =>
-      `${item.brand?.name || ""} ${item.product?.name || ""}`,
+      `${item.brand?.name || ''} ${item.product?.name || ''}`,
     meta: {
       style: {
-        width: "20%",
+        width: '20%',
       },
     },
   },
   {
-    header: "Value",
+    header: 'Value',
     accessorFn: ({ item }) => {
-      if (!item.price) return null
-      return `${currency.symbol}${item.price.toFixed(currency.decimal_digits)}`
+      if (!item.price) return null;
+      return `${currency.symbol}${item.price.toFixed(currency.decimal_digits)}`;
     },
     meta: {
       style: {
-        width: "15%",
+        width: '15%',
       },
     },
   },
   {
-    header: "Notes",
-    accessorKey: "notes",
+    header: 'Notes',
+    accessorKey: 'notes',
     cell: ({ cell }) => <NotesCell cell={cell} />,
     meta: {
-      align: "center",
+      align: 'center',
       style: {
-        textAlign: "center",
-        width: "15%",
+        textAlign: 'center',
+        width: '15%',
       },
     },
   },
   {
-    header: "Worn",
+    header: 'Worn',
     cell: ({ cell }) => <WornCell cell={cell} />,
     meta: {
-      align: "center",
-      style: { textAlign: "center", width: "5%" },
+      align: 'center',
+      style: { textAlign: 'center', width: '5%' },
     },
   },
   {
-    header: "Weight",
+    header: 'Weight',
     cell: ({ cell }) => <WeightCell cell={cell} />,
     meta: {
-      align: "right",
-      style: { textAlign: "right", width: "10%" },
+      align: 'right',
+      style: { textAlign: 'right', width: '10%' },
     },
   },
   {
-    id: "remove",
+    id: 'remove',
     cell: ({ cell }) => <RemoveItemCell cell={cell} />,
     meta: {
-      align: "center",
-      style: { textAlign: "center", width: "5%" },
+      align: 'center',
+      style: { textAlign: 'center', width: '5%' },
     },
   },
-]
+];
